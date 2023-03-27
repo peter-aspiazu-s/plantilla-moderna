c2ce0c42b54fa34ba4db620aac460410666277bf
Peter Aspiazu <paspiazusabando@gmail.com>
2023-03-27 11:02:23 -0500

    cambiando texto de experiencia por posible copywrite

--- components/experience/Experience.tsx
+++ components/experience/Experience.tsx
@@ -17,15 +17,14 @@ export const Experience: FC = () => {
             </div>
             <div className='experience-container-description'>
                 <div>
-                    Apasionado por la odontología, el Dr. Eduardo Cedeño Viteri, 
-                    desde hace 30 años ejerce la profesión en el Centro de Especialidades Odontológicas, 
-                    en Guayaquil, posicionándose en el medio, como una de las primeras opciones en los buscadores de internet. 
-                    Asociándose para ello, con especialistas en cada área para ofrecer a la comunidad, odontología multidisciplinaria. 
-                    Los especialistas estamos capacitados y calificados, con vasta experiencia de muchos años en odontología de avanzada. 
-                    Acérquese a nosotros si necesita la ayuda de profesionales experimentados que haga de su visita al dentista una experiencia agradable
-                    Somos sus socios para mantener una linda y saludable sonrisa. Estamos ubicados en una gran zona comercial de la ciudad, 
-                    cerca del aeropuerto, hospitales, centros comerciales, banca, restaurantes, etc. El edificio cuenta con seguridad, 
-                    cámaras de vigilancia y parqueaderos. Nuestras instalaciones cuenta además con WiFi, y cámaras de vigilancia.
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Inventore mollitia quisquam soluta cum consequatur odio perferendis, 
+                    error incidunt eos praesentium nulla tempora optio consequuntur quia quos sint sit nisi et.
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Inventore mollitia quisquam soluta cum consequatur odio perferendis, 
+                    error incidunt eos praesentium nulla tempora optio consequuntur quia quos sint sit nisi et.
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Inventore mollitia quisquam soluta cum consequatur odio perferendis, 
+                    error incidunt eos praesentium nulla tempora optio consequuntur quia quos sint sit nisi et.
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Inventore mollitia quisquam soluta cum consequatur odio perferendis, 
+                    error incidunt eos praesentium nulla tempora optio consequuntur quia quos sint sit nisi et.
                 </div>
             </div>
             <div className='experience-container-clients'>
